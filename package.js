a48c2606feddce4e2603b2dd89ae90f995d585de
Harrison Lynch <harrisonlynch@Harrisons-MacBook-Pro.local>
2024-05-28 22:37:14 -0700

    swap dependency versions

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
   name: 'tunguska:reactive-aggregate',
-  version: '1.3.16',
+  version: '1.3.17',
   summary: 'Publish aggregations reactively',
-  git: 'https://github.com/robfallows/tunguska-reactive-aggregate',
+  git: 'https://github.com/lynchharrison/tunguska-reactive-aggregate',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.5');
-  api.use('mongo');
+  api.versionsFrom('2.5');
+  api.use('mongo@2.0.0-rc300.2');
   api.use('ecmascript');
-  api.use('promise');
+  api.use('promise@1.0.0-rc300.1');
   api.mainModule('aggregate.js', 'server');
 });
